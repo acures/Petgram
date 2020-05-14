855b3c1a4d9505df619b5adbedeccd2d98975e80
Abel Montilla <montilla.abel@gmail.com>
2020-05-14 16:17:24 -0400

    Create PhotoCard and ListOfPhotoCards
    Installing React-Icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { GlobalStyles } from './GlobalStyles'
 import { ListOfCategories } from './components/ListOfCategories'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
       <ListOfCategories />
+      <ListOfPhotoCards />
     </>
   )
 }
---
+++ src/components/PhotoCard/styles.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+// export const = styled.article`
+// `
+
+// export const = styled.a`
+// `
+
+export const ImgWrapper = styled.div`
+  border-radius: 10px;
+  display: bloc;
+  height: 0;
+  overflow: hidden;
+  padding: 56.25% 0 0 0;
+  position: relative;
+  width: 100%;
+`
+
+export const Img = styled.img`
+  box-shadow: 0 10px 14px rgba(0, 0, 0, .2);
+  height: 100%;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  width: 100%;
+`
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  padding-top: 8px;
+
+  & svg {
+    margin-right: 4px;
+  }
+`

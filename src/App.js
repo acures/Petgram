703f427590a296a38d6aac0c7e3244ddb3bb629e
Abel Montilla <montilla.abel@gmail.com>
2020-05-14 16:31:38 -0400

    Logo with SVGR

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { GlobalStyles } from './GlobalStyles'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
+      <Logo />
       <ListOfCategories />
       <ListOfPhotoCards />
     </>

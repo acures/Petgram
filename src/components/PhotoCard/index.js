cc328a858ea4c06214ae47bd4a6e7c9afc0ce70f
Abel Montilla <montilla.abel@gmail.com>
2020-05-18 23:01:24 -0400

    Increase likes using Mutations

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
-import { ImgWrapper, Img, Button, Article } from './styles'
+import { useMutation } from '@apollo/react-hooks'
+import { ImgWrapper, Img, Article } from './styles'
 import { useLazyLoad } from '../../hooks/useLazyLoad'
 import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { LIKE_ANONYMOUS_PHOTO } from '../../graphql/likeAnonymousPhoto'
+import { FavButton } from '../FavButton'
 
 const DEFAULT_IMG = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
@@ -10,8 +12,16 @@ export const PhotoCardComponent = ({ id, likes = 0, src = DEFAULT_IMG }) => {
   const [show, ref] = useLazyLoad()
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
+  const [toggleLikePhoto] = useMutation(LIKE_ANONYMOUS_PHOTO, {
+    variables: {
+      input: { id }
+    }
+  })
 
-  const Icon = liked ? MdFavorite : MdFavoriteBorder
+  const handleFavClick = () => {
+    if (!liked) toggleLikePhoto()
+    setLiked(!liked)
+  }
 
   return (
     <Article ref={ref}>
@@ -24,9 +34,7 @@ export const PhotoCardComponent = ({ id, likes = 0, src = DEFAULT_IMG }) => {
               </ImgWrapper>
             </a>
 
-            <Button onClick={() => setLiked(!liked)}>
-              <Icon size='32px' /> {likes} likes!
-            </Button>
+            <FavButton likes={likes} liked={liked} onClick={handleFavClick} />
           </>
         )
       }
